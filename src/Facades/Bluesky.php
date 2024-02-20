30f130b7a850660de26c77ebb324e0b2b3e6838c
kawax <kawaxbiz@gmail.com>
2024-02-20 12:14:35 +0900

    Update Bluesky.php

--- src/Facades/Bluesky.php
+++ src/Facades/Bluesky.php
@@ -10,7 +10,7 @@ use Revolution\Bluesky\Contracts\Factory;
 
 /**
  * @method static static service(string $service)
- * @method static \Closure|null session(string $key)
+ * @method static mixed session(string $key)
  * @method static static login(string $identifier, string $password)
  * @method static Collection feed(string $filter = 'posts_with_replies')
  * @method static Collection timeline(string $cursor = '')
