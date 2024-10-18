b15df9d3a7cb4715158a97e2ca87c32a7f4897d8
kawax <kawaxbiz@gmail.com>
2024-10-18 18:58:25 +0900

    Update DPoP.php

--- src/Socalite/DPoP.php
+++ src/Socalite/DPoP.php
@@ -19,10 +19,6 @@ class DPoP
 
     public static function proof(array $payload, JsonWebKey $jwk): string
     {
-        if (empty($payload['nonce'])) {
-            return '';
-        }
-
         $pub_jwk = $jwk->asPublic();
 
         $head = [
@@ -30,15 +26,6 @@ class DPoP
             'alg' => JsonWebKey::ALG,
             'jwk' => $pub_jwk->toArray(),
         ];
-
-//        $payload = [
-//            'nonce' => $nonce,
-//            'htm' => 'POST',
-//            'htu' => $par_url,
-//            'jti' => Str::random(40),
-//            'iat' => now()->timestamp,
-//            'exp' => now()->addSeconds(30)->timestamp,
-//        ];
 
         return JsonWebToken::encode($head, $payload, $jwk->key());
     }
