b1b4ed85f64b3e9d0d2c87b8db17a04e9212c62c
kawax <kawaxbiz@gmail.com>
2024-10-18 21:10:24 +0900

    Update DPoP.php

--- src/Socalite/DPoP.php
+++ src/Socalite/DPoP.php
@@ -17,7 +17,7 @@ class DPoP
         return BlueskyKey::load($key)->toJWK();
     }
 
-    public static function proof(array $payload, JsonWebKey $jwk): string
+    public static function authProof(array $payload, JsonWebKey $jwk): string
     {
         $pub_jwk = $jwk->asPublic();
 
@@ -25,6 +25,34 @@ class DPoP
             'typ' => 'dpop+jwt',
             'alg' => JsonWebKey::ALG,
             'jwk' => $pub_jwk->toArray(),
+        ];
+
+        return JsonWebToken::encode($head, $payload, $jwk->key());
+    }
+
+    public static function apiProof(
+        string $nonce,
+        string $method,
+        string $url,
+        string $iss,
+        string $code,
+        JsonWebKey $jwk): string
+    {
+        $head = [
+            'typ' => 'dpop+jwt',
+            'alg' => JsonWebKey::ALG,
+            'jwk' => $jwk->asPublic()->toArray(),
+        ];
+
+        $payload = [
+            'nonce' => $nonce,
+            'iss' => $iss,
+            'htu' => $url,
+            'htm' => $method,
+            'jti' => Str::random(40),
+            'iat' => now()->timestamp,
+            'exp' => now()->addSeconds(30)->timestamp,
+            'ath' => self::createCodeChallenge($code),
         ];
 
         return JsonWebToken::encode($head, $payload, $jwk->key());
