81a5a38e81a2e897667a41adfb4c309de9cefe5d
kawax <kawaxbiz@gmail.com>
2024-10-19 18:19:34 +0900

    wip

--- src/Agent/OAuthAgent.php
+++ src/Agent/OAuthAgent.php
@@ -30,7 +30,7 @@ class OAuthAgent implements Agent
     public function http(bool $auth = true): PendingRequest
     {
         return Http::baseUrl($this->baseUrl($auth))
-            ->when($auth, $this->dpop(...));
+            ->when(true, $this->dpop(...));
     }
 
     protected function dpop(PendingRequest $http): PendingRequest
@@ -42,7 +42,7 @@ class OAuthAgent implements Agent
                     iss: $this->session('iss'),
                     url: $request->getUri(),
                     token: $this->token(),
-                    nonce: $this->session('dpop_nonce', ''),
+                    nonce: $this->session('dpop_api_nonce', ''),
                     method: $request->getMethod(),
                 );
 
@@ -50,7 +50,7 @@ class OAuthAgent implements Agent
             })->withResponseMiddleware(function (ResponseInterface $response) {
                 $dpop_nonce = collect($response->getHeader('DPoP-Nonce'))->first();
 
-                $this->session->put('dpop_nonce', $dpop_nonce);
+                $this->session->put('dpop_api_nonce', $dpop_nonce);
 
                 return $response;
             })->retry(times: 2, throw: false);
