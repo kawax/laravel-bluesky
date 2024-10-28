9996739e5a51b99ad4f4ed0308c20ea01b8a4439
kawax <kawaxbiz@gmail.com>
2024-10-28 16:26:09 +0900

    Update bluesky.php

--- config/bluesky.php
+++ config/bluesky.php
@@ -14,20 +14,20 @@ return [
         'metadata' => [
             'scope' => env('BLUESKY_OAUTH_SCOPE', 'atproto transition:generic'),
 
-            'client_name' => config('app.name'),
-            'client_uri' => config('app.url'),
-
             'grant_types' => ['authorization_code', 'refresh_token'],
             'response_types' => ['code'],
             'application_type' => env('BLUESKY_OAUTH_APPLICATION_TYPE', 'web'),
             'token_endpoint_auth_method' => env('BLUESKY_OAUTH_TOKEN_METHOD', 'private_key_jwt'),
             'token_endpoint_auth_signing_alg' => env('BLUESKY_OAUTH_TOKEN_SIGN', 'ES256'),
 
+            'dpop_bound_access_tokens' => env('BLUESKY_OAUTH_DPOP', true),
+
+            // Optional fields
+            'client_name' => env('BLUESKY_OAUTH_CLIENT_NAME'),
+            'client_uri' => env('BLUESKY_OAUTH_CLIENT_URI'),
             'logo_uri' => env('BLUESKY_OAUTH_LOGO'),
             'tos_uri' => env('BLUESKY_OAUTH_TOS'),
             'policy_uri' => env('BLUESKY_OAUTH_PRIVACY'),
-
-            'dpop_bound_access_tokens' => env('BLUESKY_OAUTH_DPOP', true),
         ],
 
         // Private key(base64 encoded)
