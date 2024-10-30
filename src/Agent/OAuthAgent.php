4ef3c15d3843b28df20eb7eed9ba5a45dac57218
kawax <kawaxbiz@gmail.com>
2024-10-30 19:49:28 +0900

    Update OAuthAgent.php

--- src/Agent/OAuthAgent.php
+++ src/Agent/OAuthAgent.php
@@ -115,7 +115,7 @@ final class OAuthAgent implements Agent
         }
 
         $this->session->put('didDoc', Bluesky::identity()->resolveDID($did)->json());
-        $this->session->put('profile', Bluesky::withAgent($this)->profile($did)->json());
+        $this->session->put('profile', Bluesky::withToken($this->session)->profile($did)->json());
 
         if (! $this->session->has('iss') && ! empty($pds_url = $this->pdsUrl())) {
             $pds = Bluesky::pds()->resource($pds_url);
