5e7384491d71d8d8426fc088c60461d993abdb4b
kawax <kawaxbiz@gmail.com>
2024-11-01 20:00:40 +0900

    Update OAuthConfig.php

--- src/Socalite/OAuthConfig.php
+++ src/Socalite/OAuthConfig.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Revolution\Bluesky\Socalite;
 
 use Closure;
+use Illuminate\Support\Facades\Route;
 use Revolution\Bluesky\Socalite\Key\JsonWebKeySet;
 
 class OAuthConfig
@@ -24,12 +25,18 @@ class OAuthConfig
             return call_user_func(static::$metadataUsing);
         }
 
+        if (Route::has('bluesky.oauth.redirect')) {
+            $redirect = route('bluesky.oauth.redirect');
+        } else {
+            $redirect = 'http://127.0.0.1:8000/';
+        }
+
         return collect(config('bluesky.oauth.metadata'))
             ->merge(
                 [
                     'client_id' => route('bluesky.oauth.client-metadata'),
                     'jwks_uri' => route('bluesky.oauth.jwks'),
-                    'redirect_uris' => [url(config('bluesky.oauth.socialite.redirect', 'http://127.0.0.1:8000/'))],
+                    'redirect_uris' => [url(config('bluesky.oauth.redirect') ?? $redirect)],
                 ],
             )->reject(fn ($item) => is_null($item))
             ->toArray();
