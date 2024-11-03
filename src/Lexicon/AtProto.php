9d779ee369a00d84131c8acf535a1729d4127252
kawax <kawaxbiz@gmail.com>
2024-11-03 09:07:01 +0900

    lexicon

--- src/Lexicon/AtProto.php
+++ src/Lexicon/AtProto.php
@@ -6,63 +6,298 @@ namespace Revolution\Bluesky\Lexicon;
 
 enum AtProto: string
 {
+    /**
+     * Delete an actor's account with a token and password. Can only be called after requesting a deletion token. Requires auth.
+     */
     case deleteAccount = 'com.atproto.server.deleteAccount';
+
+    /**
+     * Disable an account from receiving new invite codes, but does not invalidate existing codes.
+     */
     case disableAccountInvites = 'com.atproto.admin.disableAccountInvites';
+
+    /**
+     * Disable some set of codes and/or all codes associated with a set of users.
+     */
     case disableInviteCodes = 'com.atproto.admin.disableInviteCodes';
+
+    /**
+     * Re-enable an account's ability to receive invite codes.
+     */
     case enableAccountInvites = 'com.atproto.admin.enableAccountInvites';
+
+    /**
+     * Get details about an account.
+     */
     case getAccountInfo = 'com.atproto.admin.getAccountInfo';
+
+    /**
+     * Get details about some accounts.
+     */
     case getAccountInfos = 'com.atproto.admin.getAccountInfos';
+
+    /**
+     * Get an admin view of invite codes.
+     */
     case getInviteCodes = 'com.atproto.admin.getInviteCodes';
+
+    /**
+     * Get the service-specific admin status of a subject (account, record, or blob).
+     */
     case getSubjectStatus = 'com.atproto.admin.getSubjectStatus';
+
+    /**
+     * Get list of accounts that matches your search query.
+     */
     case searchAccounts = 'com.atproto.admin.searchAccounts';
+
+    /**
+     * Send email to a user's account email address.
+     */
     case sendEmail = 'com.atproto.admin.sendEmail';
+
+    /**
+     * Administrative action to update an account's email.
+     */
     case updateAccountEmail = 'com.atproto.admin.updateAccountEmail';
+
+    /**
+     * Administrative action to update an account's handle.
+     */
     case updateAccountHandle = 'com.atproto.admin.updateAccountHandle';
+
+    /**
+     * Update the password for a user account as an administrator.
+     */
     case updateAccountPassword = 'com.atproto.admin.updateAccountPassword';
+
+    /**
+     * Update the service-specific admin status of a subject (account, record, or blob).
+     */
     case updateSubjectStatus = 'com.atproto.admin.updateSubjectStatus';
+
+    /**
+     * Describe the credentials that should be included in the DID doc of an account that is migrating to this service.
+     */
     case getRecommendedDidCredentials = 'com.atproto.identity.getRecommendedDidCredentials';
+
+    /**
+     * Request an email with a code to in order to request a signed PLC operation. Requires Auth.
+     */
     case requestPlcOperationSignature = 'com.atproto.identity.requestPlcOperationSignature';
+
+    /**
+     * Resolves a handle (domain name) to a DID.
+     */
     case resolveHandle = 'com.atproto.identity.resolveHandle';
+
+    /**
+     * Signs a PLC operation to update some value(s) in the requesting DID's document.
+     */
     case signPlcOperation = 'com.atproto.identity.signPlcOperation';
+
+    /**
+     * Validates a PLC operation to ensure that it doesn't violate a service's constraints or get the identity into a bad state, then submits it to the PLC registry
+     */
     case submitPlcOperation = 'com.atproto.identity.submitPlcOperation';
+
+    /**
+     * Updates the current account's handle. Verifies handle validity, and updates did:plc document if necessary. Implemented by PDS, and requires auth.
+     */
     case updateHandle = 'com.atproto.identity.updateHandle';
+
+    /**
+     * Find labels relevant to the provided AT-URI patterns. Public endpoint for moderation services, though may return different or additional results with auth.
+     */
     case queryLabels = 'com.atproto.label.queryLabels';
+
+    /**
+     * Submit a moderation report regarding an atproto account or record. Implemented by moderation services (with PDS proxying), and requires auth.
+     */
     case createReport = 'com.atproto.moderation.createReport';
+
+    /**
+     * Apply a batch transaction of repository creates, updates, and deletes. Requires auth, implemented by PDS.
+     */
     case applyWrites = 'com.atproto.repo.applyWrites';
+
+    /**
+     * Create a single new repository record. Requires auth, implemented by PDS.
+     */
     case createRecord = 'com.atproto.repo.createRecord';
+
+    /**
+     * Delete a repository record, or ensure it doesn't exist. Requires auth, implemented by PDS.
+     */
     case deleteRecord = 'com.atproto.repo.deleteRecord';
+
+    /**
+     * Get information about an account and repository, including the list of collections. Does not require auth.
+     */
     case describeRepo = 'com.atproto.repo.describeRepo';
+
+    /**
+     * Get a single record from a repository. Does not require auth.
+     */
     case getRecord = 'com.atproto.repo.getRecord';
+
+    /**
+     * Import a repo in the form of a CAR file. Requires Content-Length HTTP header to be set.
+     */
     case importRepo = 'com.atproto.repo.importRepo';
+
+    /**
+     * Returns a list of missing blobs for the requesting account. Intended to be used in the account migration flow.
+     */
     case listMissingBlobs = 'com.atproto.repo.listMissingBlobs';
+
+    /**
+     * List a range of records in a repository, matching a specific collection. Does not require auth.
+     */
     case listRecords = 'com.atproto.repo.listRecords';
+
+    /**
+     * Write a repository record, creating or updating it as needed. Requires auth, implemented by PDS.
+     */
     case putRecord = 'com.atproto.repo.putRecord';
+
+    /**
+     * Upload a new blob, to be referenced from a repository record. The blob will be deleted if it is not referenced within a time window (eg, minutes). Blob restrictions (mimetype, size, etc) are enforced when the reference is created. Requires auth, implemented by PDS.
+     */
     case uploadBlob = 'com.atproto.repo.uploadBlob';
+
+    /**
+     * Activates a currently deactivated account. Used to finalize account migration after the account's repo is imported and identity is setup.
+     */
     case activateAccount = 'com.atproto.server.activateAccount';
+
+    /**
+     * Returns the status of an account, especially as pertaining to import or recovery. Can be called many times over the course of an account migration. Requires auth and can only be called pertaining to oneself.
+     */
     case checkAccountStatus = 'com.atproto.server.checkAccountStatus';
+
+    /**
+     * Confirm an email using a token from com.atproto.server.requestEmailConfirmation.
+     */
     case confirmEmail = 'com.atproto.server.confirmEmail';
+
+    /**
+     * Create an account. Implemented by PDS.
+     */
     case createAccount = 'com.atproto.server.createAccount';
+
+    /**
+     * Create an App Password.
+     */
     case createAppPassword = 'com.atproto.server.createAppPassword';
+
+    /**
+     * Create an invite code.
+     */
     case createInviteCode = 'com.atproto.server.createInviteCode';
+
+    /**
+     * Create invite codes.
+     */
     case createInviteCodes = 'com.atproto.server.createInviteCodes';
+
+    /**
+     * Create an authentication session.
+     */
     case createSession = 'com.atproto.server.createSession';
+
+    /**
+     * Deactivates a currently active account. Stops serving of repo, and future writes to repo until reactivated. Used to finalize account migration with the old host after the account has been activated on the new host.
+     */
     case deactivateAccount = 'com.atproto.server.deactivateAccount';
+
+    /**
+     * Delete the current session. Requires auth.
+     */
     case deleteSession = 'com.atproto.server.deleteSession';
+
+    /**
+     * Describes the server's account creation requirements and capabilities. Implemented by PDS.
+     */
     case describeServer = 'com.atproto.server.describeServer';
+
+    /**
+     * Get all invite codes for the current account. Requires auth.
+     */
     case getAccountInviteCodes = 'com.atproto.server.getAccountInviteCodes';
+
+    /**
+     * Get a signed token on behalf of the requesting DID for the requested service.
+     */
     case getServiceAuth = 'com.atproto.server.getServiceAuth';
+
+    /**
+     * Get information about the current auth session. Requires auth.
+     */
     case getSession = 'com.atproto.server.getSession';
+
+    /**
+     * List all App Passwords.
+     */
     case listAppPasswords = 'com.atproto.server.listAppPasswords';
+
+    /**
+     * Refresh an authentication session. Requires auth using the 'refreshJwt' (not the 'accessJwt').
+     */
     case refreshSession = 'com.atproto.server.refreshSession';
+
+    /**
+     * Initiate a user account deletion via email.
+     */
     case requestAccountDelete = 'com.atproto.server.requestAccountDelete';
+
+    /**
+     * Request an email with a code to confirm ownership of email.
+     */
     case requestEmailConfirmation = 'com.atproto.server.requestEmailConfirmation';
+
+    /**
+     * Request a token in order to update email.
+     */
     case requestEmailUpdate = 'com.atproto.server.requestEmailUpdate';
+
+    /**
+     * Initiate a user account password reset via email.
+     */
     case requestPasswordReset = 'com.atproto.server.requestPasswordReset';
+
+    /**
+     * Reserve a repo signing key, for use with account creation. Necessary so that a DID PLC update operation can be constructed during an account migraiton. Public and does not require auth; implemented by PDS. NOTE: this endpoint may change when full account migration is implemented.
+     */
     case reserveSigningKey = 'com.atproto.server.reserveSigningKey';
+
+    /**
+     * Reset a user account password using a token.
+     */
     case resetPassword = 'com.atproto.server.resetPassword';
+
+    /**
+     * Revoke an App Password by name.
+     */
     case revokeAppPassword = 'com.atproto.server.revokeAppPassword';
+
+    /**
+     * Update an account's email.
+     */
     case updateEmail = 'com.atproto.server.updateEmail';
+
+    /**
+     * Check accounts location in signup queue.
+     */
     case checkSignupQueue = 'com.atproto.temp.checkSignupQueue';
+
+    /**
+     * DEPRECATED: use queryLabels or subscribeLabels instead -- Fetch all labels from a labeler created after a certain date.
+     */
     case fetchLabels = 'com.atproto.temp.fetchLabels';
+
+    /**
+     * Request a verification code to be sent to the supplied phone number
+     */
     case requestPhoneVerification = 'com.atproto.temp.requestPhoneVerification';
 }
--- src/Lexicon/Bsky.php
+++ src/Lexicon/Bsky.php
@@ -6,63 +6,298 @@ namespace Revolution\Bluesky\Lexicon;
 
 enum Bsky: string
 {
+    /**
+     * Get private preferences attached to the current account. Expected use is synchronization between multiple devices, and import/export during account migration. Requires auth.
+     */
     case getPreferences = 'app.bsky.actor.getPreferences';
+
+    /**
+     * Get detailed profile view of an actor. Does not require auth, but contains relevant metadata with auth.
+     */
     case getProfile = 'app.bsky.actor.getProfile';
+
+    /**
+     * Get detailed profile views of multiple actors.
+     */
     case getProfiles = 'app.bsky.actor.getProfiles';
+
+    /**
+     * Get a list of suggested actors. Expected use is discovery of accounts to follow during new account onboarding.
+     */
     case getSuggestions = 'app.bsky.actor.getSuggestions';
+
+    /**
+     * Set notification-related preferences for an account. Requires auth.
+     */
     case putPreferences = 'app.bsky.notification.putPreferences';
+
+    /**
+     * Find actors (profiles) matching search criteria. Does not require auth.
+     */
     case searchActors = 'app.bsky.actor.searchActors';
+
+    /**
+     * Find actor suggestions for a prefix search term. Expected use is for auto-completion during text field entry. Does not require auth.
+     */
     case searchActorsTypeahead = 'app.bsky.actor.searchActorsTypeahead';
+
+    /**
+     * Get information about a feed generator, including policies and offered feed URIs. Does not require auth; implemented by Feed Generator services (not App View).
+     */
     case describeFeedGenerator = 'app.bsky.feed.describeFeedGenerator';
+
+    /**
+     * Get a list of feeds (feed generator records) created by the actor (in the actor's repo).
+     */
     case getActorFeeds = 'app.bsky.feed.getActorFeeds';
+
+    /**
+     * Get a list of posts liked by an actor. Requires auth, actor must be the requesting account.
+     */
     case getActorLikes = 'app.bsky.feed.getActorLikes';
+
+    /**
+     * Get a view of an actor's 'author feed' (post and reposts by the author). Does not require auth.
+     */
     case getAuthorFeed = 'app.bsky.feed.getAuthorFeed';
+
+    /**
+     * Get a hydrated feed from an actor's selected feed generator. Implemented by App View.
+     */
     case getFeed = 'app.bsky.feed.getFeed';
+
+    /**
+     * Get information about a feed generator. Implemented by AppView.
+     */
     case getFeedGenerator = 'app.bsky.feed.getFeedGenerator';
+
+    /**
+     * Get information about a list of feed generators.
+     */
     case getFeedGenerators = 'app.bsky.feed.getFeedGenerators';
+
+    /**
+     * Get a skeleton of a feed provided by a feed generator. Auth is optional, depending on provider requirements, and provides the DID of the requester. Implemented by Feed Generator Service.
+     */
     case getFeedSkeleton = 'app.bsky.feed.getFeedSkeleton';
+
+    /**
+     * Get like records which reference a subject (by AT-URI and CID).
+     */
     case getLikes = 'app.bsky.feed.getLikes';
+
+    /**
+     * Get a feed of recent posts from a list (posts and reposts from any actors on the list). Does not require auth.
+     */
     case getListFeed = 'app.bsky.feed.getListFeed';
+
+    /**
+     * Get posts in a thread. Does not require auth, but additional metadata and filtering will be applied for authed requests.
+     */
     case getPostThread = 'app.bsky.feed.getPostThread';
+
+    /**
+     * Gets post views for a specified list of posts (by AT-URI). This is sometimes referred to as 'hydrating' a 'feed skeleton'.
+     */
     case getPosts = 'app.bsky.feed.getPosts';
+
+    /**
+     * Get a list of quotes for a given post.
+     */
     case getQuotes = 'app.bsky.feed.getQuotes';
+
+    /**
+     * Get a list of reposts for a given post.
+     */
     case getRepostedBy = 'app.bsky.feed.getRepostedBy';
+
+    /**
+     * Get a list of suggested feeds (feed generators) for the requesting account.
+     */
     case getSuggestedFeeds = 'app.bsky.feed.getSuggestedFeeds';
+
+    /**
+     * Get a view of the requesting account's home timeline. This is expected to be some form of reverse-chronological feed.
+     */
     case getTimeline = 'app.bsky.feed.getTimeline';
+
+    /**
+     * Find posts matching search criteria, returning views of those posts.
+     */
     case searchPosts = 'app.bsky.feed.searchPosts';
+
+    /**
+     * Send information about interactions with feed items back to the feed generator that served them.
+     */
     case sendInteractions = 'app.bsky.feed.sendInteractions';
+
+    /**
+     * Get a list of starter packs created by the actor.
+     */
     case getActorStarterPacks = 'app.bsky.graph.getActorStarterPacks';
+
+    /**
+     * Enumerates which accounts the requesting account is currently blocking. Requires auth.
+     */
     case getBlocks = 'app.bsky.graph.getBlocks';
+
+    /**
+     * Enumerates accounts which follow a specified account (actor).
+     */
     case getFollowers = 'app.bsky.graph.getFollowers';
+
+    /**
+     * Enumerates accounts which a specified account (actor) follows.
+     */
     case getFollows = 'app.bsky.graph.getFollows';
+
+    /**
+     * Enumerates accounts which follow a specified account (actor) and are followed by the viewer.
+     */
     case getKnownFollowers = 'app.bsky.graph.getKnownFollowers';
+
+    /**
+     * Gets a 'view' (with additional context) of a specified list.
+     */
     case getList = 'app.bsky.graph.getList';
+
+    /**
+     * Get mod lists that the requesting account (actor) is blocking. Requires auth.
+     */
     case getListBlocks = 'app.bsky.graph.getListBlocks';
+
+    /**
+     * Enumerates mod lists that the requesting account (actor) currently has muted. Requires auth.
+     */
     case getListMutes = 'app.bsky.graph.getListMutes';
+
+    /**
+     * Enumerates the lists created by a specified account (actor).
+     */
     case getLists = 'app.bsky.graph.getLists';
+
+    /**
+     * Enumerates accounts that the requesting account (actor) currently has muted. Requires auth.
+     */
     case getMutes = 'app.bsky.graph.getMutes';
+
+    /**
+     * Enumerates public relationships between one account, and a list of other accounts. Does not require auth.
+     */
     case getRelationships = 'app.bsky.graph.getRelationships';
+
+    /**
+     * Gets a view of a starter pack.
+     */
     case getStarterPack = 'app.bsky.graph.getStarterPack';
+
+    /**
+     * Get views for a list of starter packs.
+     */
     case getStarterPacks = 'app.bsky.graph.getStarterPacks';
+
+    /**
+     * Enumerates follows similar to a given account (actor). Expected use is to recommend additional accounts immediately after following one account.
+     */
     case getSuggestedFollowsByActor = 'app.bsky.graph.getSuggestedFollowsByActor';
+
+    /**
+     * Creates a mute relationship for the specified account. Mutes are private in Bluesky. Requires auth.
+     */
     case muteActor = 'app.bsky.graph.muteActor';
+
+    /**
+     * Creates a mute relationship for the specified list of accounts. Mutes are private in Bluesky. Requires auth.
+     */
     case muteActorList = 'app.bsky.graph.muteActorList';
+
+    /**
+     * Mutes a thread preventing notifications from the thread and any of its children. Mutes are private in Bluesky. Requires auth.
+     */
     case muteThread = 'app.bsky.graph.muteThread';
+
+    /**
+     * Unmutes the specified account. Requires auth.
+     */
     case unmuteActor = 'app.bsky.graph.unmuteActor';
+
+    /**
+     * Unmutes the specified list of accounts. Requires auth.
+     */
     case unmuteActorList = 'app.bsky.graph.unmuteActorList';
+
+    /**
+     * Unmutes the specified thread. Requires auth.
+     */
     case unmuteThread = 'app.bsky.graph.unmuteThread';
+
+    /**
+     * Get information about a list of labeler services.
+     */
     case getServices = 'app.bsky.labeler.getServices';
+
+    /**
+     * Count the number of unread notifications for the requesting account. Requires auth.
+     */
     case getUnreadCount = 'app.bsky.notification.getUnreadCount';
+
+    /**
+     * Enumerate notifications for the requesting account. Requires auth.
+     */
     case listNotifications = 'app.bsky.notification.listNotifications';
+
+    /**
+     * Register to receive push notifications, via a specified service, for the requesting account. Requires auth.
+     */
     case registerPush = 'app.bsky.notification.registerPush';
+
+    /**
+     * Notify server that the requesting account has seen notifications. Requires auth.
+     */
     case updateSeen = 'app.bsky.notification.updateSeen';
+
+    /**
+     * Get miscellaneous runtime configuration.
+     */
     case getConfig = 'app.bsky.unspecced.getConfig';
+
+    /**
+     * An unspecced view of globally popular feed generators.
+     */
     case getPopularFeedGenerators = 'app.bsky.unspecced.getPopularFeedGenerators';
+
+    /**
+     * Get a skeleton of suggested actors. Intended to be called and then hydrated through app.bsky.actor.getSuggestions
+     */
     case getSuggestionsSkeleton = 'app.bsky.unspecced.getSuggestionsSkeleton';
+
+    /**
+     * Get a list of suggestions (feeds and users) tagged with categories
+     */
     case getTaggedSuggestions = 'app.bsky.unspecced.getTaggedSuggestions';
+
+    /**
+     * Backend Actors (profile) search, returns only skeleton.
+     */
     case searchActorsSkeleton = 'app.bsky.unspecced.searchActorsSkeleton';
+
+    /**
+     * Backend Posts search, returns only skeleton
+     */
     case searchPostsSkeleton = 'app.bsky.unspecced.searchPostsSkeleton';
+
+    /**
+     * Get status details for a video processing job.
+     */
     case getJobStatus = 'app.bsky.video.getJobStatus';
+
+    /**
+     * Get video upload limits for the authenticated user.
+     */
     case getUploadLimits = 'app.bsky.video.getUploadLimits';
+
+    /**
+     * Upload a video to be processed then stored on the PDS.
+     */
     case uploadVideo = 'app.bsky.video.uploadVideo';
 }
