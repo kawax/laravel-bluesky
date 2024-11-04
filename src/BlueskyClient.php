7ba57c3f0e149b8e23a77e4d0410557d33fa9bef
kawax <kawaxbiz@gmail.com>
2024-11-05 08:35:57 +0900

    Update BlueskyClient.php

--- src/BlueskyClient.php
+++ src/BlueskyClient.php
@@ -6,24 +6,17 @@ namespace Revolution\Bluesky;
 
 use BackedEnum;
 use Illuminate\Container\Container;
-use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
-use InvalidArgumentException;
 use Revolution\Bluesky\Agent\LegacyAgent;
 use Revolution\Bluesky\Agent\OAuthAgent;
-use Revolution\Bluesky\Client\AtpClient;
-use Revolution\Bluesky\Client\BskyClient;
 use Revolution\Bluesky\Contracts\Agent;
 use Revolution\Bluesky\Contracts\Factory;
-use Revolution\Bluesky\Lexicon\Contracts\App\Bsky\Video;
-use Revolution\Bluesky\Lexicon\Contracts\Com\Atproto\Repo;
 use Revolution\Bluesky\Lexicon\Contracts\Com\Atproto\Server;
-use Revolution\Bluesky\Notifications\BlueskyMessage;
 use Revolution\Bluesky\Session\LegacySession;
 use Revolution\Bluesky\Session\OAuthSession;
 use Revolution\Bluesky\Support\Identity;
@@ -33,6 +26,7 @@ use function Illuminate\Support\enum_value;
 
 class BlueskyClient implements Factory
 {
+    use HasShortHand;
     use Macroable;
     use Conditionable;
 
@@ -99,121 +93,11 @@ class BlueskyClient implements Factory
         return $this->http($auth)->$method(enum_value($api), $params);
     }
 
-    protected function bsky(bool $auth = false): BskyClient
-    {
-        return app(BskyClient::class)->withHttp($this->http($auth));
-    }
-
-    protected function atp(bool $auth = true): AtpClient
-    {
-        return app(AtpClient::class)->withHttp($this->http($auth));
-    }
-
-    /**
-     * @param  string|null  $actor  DID or handle.
-     */
-    public function profile(?string $actor = null): Response
-    {
-        return $this->bsky(auth: false)->getProfile(
-            actor: $actor ?? $this->agent()?->did() ?? '',
-        );
-    }
-
-    /**
-     * getAuthorFeed.
-     *
-     * @param  string|null  $actor  DID or handle.
-     */
-    public function feed(?string $actor = null, int $limit = 50, string $cursor = '', string $filter = 'posts_with_replies'): Response
-    {
-        return $this->bsky(auth: false)->getAuthorFeed(
-            actor: $actor ?? $this->agent()?->did() ?? '',
-            limit: $limit,
-            cursor: $cursor,
-            filter: $filter,
-        );
-    }
-
-    /**
-     * My timeline.
-     */
-    public function timeline(int $limit = 50, string $cursor = ''): Response
-    {
-        return $this->bsky(auth: true)->getTimeline(
-            limit: $limit,
-            cursor: $cursor,
-        );
-    }
-
-    public function createRecord(string $repo, string $collection, array $record): Response
-    {
-        return $this->atp(auth: true)->createRecord(
-            repo: $repo,
-            collection: $collection,
-            record: $record,
-        );
-    }
-
-    /**
-     * Create new post.
-     */
-    public function post(string|BlueskyMessage $text): Response
-    {
-        $message = $text instanceof BlueskyMessage ? $text : BlueskyMessage::create($text);
-
-        $record = collect($message->toArray())
-            ->put('createdAt', now()->toISOString())
-            ->reject(fn ($item) => blank($item))
-            ->toArray();
-
-        return $this->createRecord(
-            repo: $this->agent()->did(),
-            collection: 'app.bsky.feed.post',
-            record: $record,
-        );
-    }
-
-    /**
-     * Upload blob.
-     *
-     * @throws ConnectionException
-     */
-    public function uploadBlob(mixed $data, string $type = 'image/png'): Response
-    {
-        return $this->http()
-            ->withBody($data, $type)
-            ->post(Repo::uploadBlob);
-    }
-
-    /**
-     * Upload video.
-     *
-     * @throws ConnectionException
-     */
-    public function uploadVideo(mixed $data, string $type = 'video/mp4'): Response
-    {
-        return $this->http()
-            ->withBody($data, $type)
-            ->post(Video::uploadVideo);
-    }
-
     public function refreshSession(): self
     {
         $this->agent = $this->agent()?->refreshSession();
 
         return $this;
-    }
-
-    /**
-     * @param  string  $handle  e.g. "alice.test"
-     */
-    public function resolveHandle(string $handle): Response
-    {
-        if (! Identity::isHandle($handle)) {
-            throw new InvalidArgumentException("The handle '$handle' is not a valid handle.");
-        }
-
-        return $this->atp(auth: false)->resolveHandle(handle: $handle);
     }
 
     public function identity(): Identity
