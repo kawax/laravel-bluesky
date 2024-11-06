6dc9f6ce2c884aa311c63e723555c3501e1ff5fc
kawax <kawaxbiz@gmail.com>
2024-11-06 16:51:03 +0900

    Create Video.php

---
+++ src/Embed/Video.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Revolution\Bluesky\Embed;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Revolution\AtProto\Lexicon\Enum\Embed;
+
+final readonly class Video implements Arrayable
+{
+    public function __construct(
+        private string $video,
+        private ?string $alt = null,
+        private ?array $captions = null,
+        private ?array $aspectRatio = null,
+    ) {
+    }
+
+    public static function create(string $video, ?string $alt = null, ?array $captions = null, ?array $aspectRatio = null): self
+    {
+        return new self(...func_get_args());
+    }
+
+    public function toArray(): array
+    {
+        return [
+            '$type' => Embed::Video->value,
+            'video' => collect([
+                'video' => $this->video,
+                'alt' => $this->alt,
+                'captions' => $this->captions,
+                'aspectRatio' => $this->aspectRatio,
+            ])->reject(fn ($item) => is_null($item))
+                ->toArray(),
+        ];
+    }
+}
