083744bf926bb15a5f1cfc80645f107fcfe1555a
kawax <kawaxbiz@gmail.com>
2024-11-07 13:43:16 +0900

    Update OAuthAgentTest.php

--- tests/Feature/Agent/OAuthAgentTest.php
+++ tests/Feature/Agent/OAuthAgentTest.php
@@ -95,7 +95,7 @@ class OAuthAgentTest extends TestCase
         Bluesky::shouldReceive('identity->resolveDID->json')->andReturn([
             'service' => [['id' => '#atproto_pds', 'serviceEndpoint' => 'https://pds']],
         ]);
-        Bluesky::shouldReceive('profile->json')->once()->andReturn([
+        Bluesky::shouldReceive('getProfile->json')->once()->andReturn([
             'handle' => 'handle',
         ]);
         Bluesky::shouldReceive('pds->resource')->once()->andReturn(ProtectedResource::create([
