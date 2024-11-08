3544c688dd738a9cb9052ead35e2eca3490ccf87
kawax <kawaxbiz@gmail.com>
2024-11-08 21:27:48 +0900

    Update Factory.php

--- src/Contracts/Factory.php
+++ src/Contracts/Factory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Revolution\Bluesky\Contracts;
 
 use BackedEnum;
-use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 use Revolution\Bluesky\Client\AtpClient;
 use Revolution\Bluesky\Session\OAuthSession;
@@ -16,13 +15,11 @@ interface Factory
 
     public function login(string $identifier, string $password): self;
 
-    public function agent(): ?Agent;
-
-    public function withAgent(?Agent $agent): self;
-
-    public function http(bool $auth = true): PendingRequest;
+    public function client(bool $auth = true): XrpcClient|AtpClient;
 
     public function send(BackedEnum|string $api, string $method = 'get', bool $auth = true, ?array $params = null, ?callable $callback = null): Response;
 
-    public function client(bool $auth = true): XrpcClient|AtpClient;
+    public function agent(): ?Agent;
+
+    public function withAgent(?Agent $agent): self;
 }
