5fdd3615e57a41754a1d155402c20bfd3c3d721a
kawax <kawaxbiz@gmail.com>
2024-11-08 20:42:28 +0900

    Create ValidationTest.php

---
+++ tests/Feature/Validation/ValidationTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature\Validation;
+
+use Illuminate\Support\Facades\Validator;
+use Revolution\Bluesky\Validation\AtActor;
+use Revolution\Bluesky\Validation\AtDID;
+use Revolution\Bluesky\Validation\AtHandle;
+use Tests\TestCase;
+
+class ValidationTest extends TestCase
+{
+    public function test_did_passes()
+    {
+        $validator = Validator::make(
+            data: ['did' => 'did:plc:test'],
+            rules: ['did' => new AtDID()],
+        );
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function test_did_fails()
+    {
+        $validator = Validator::make(
+            data: ['did' => ''],
+            rules: ['did' => new AtDID()],
+        );
+
+        $this->assertTrue($validator->fails());
+    }
+
+    public function test_handle_passes()
+    {
+        $validator = Validator::make(
+            data: ['handle' => 'alice.local'],
+            rules: ['handle' => new AtHandle()],
+        );
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function test_handle_fails()
+    {
+        $validator = Validator::make(
+            data: ['handle' => ''],
+            rules: ['handle' => new AtHandle()],
+        );
+
+        $this->assertTrue($validator->fails());
+    }
+
+    public function test_actor_passes()
+    {
+        $validator = Validator::make(
+            data: [
+                'did' => 'did:plc:test',
+                'handle' => 'alice.local',
+            ],
+            rules: [
+                'did' => [new AtActor()],
+                'handle' => [new AtActor()],
+            ],
+        );
+
+        $this->assertTrue($validator->passes());
+    }
+
+    public function test_did_or_handle_fails()
+    {
+        $validator = Validator::make(
+            data: [
+                'did' => '',
+                'handle' => null,
+            ],
+            rules: [
+                'did' => [new AtActor()],
+                'handle' => [new AtActor()],
+            ],
+        );
+
+        $this->assertTrue($validator->fails());
+    }
+}
