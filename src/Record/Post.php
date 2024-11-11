329e9ff41e156f8304150eb2d959a275f3882d19
kawax <kawaxbiz@gmail.com>
2024-11-11 17:34:37 +0900

    Update Post.php

--- src/Record/Post.php
+++ src/Record/Post.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Revolution\Bluesky\Record;
 
 use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Support\Str;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
-use Revolution\AtProto\Lexicon\Attributes\Format;
-use Revolution\AtProto\Lexicon\Enum\Facet;
 use Revolution\AtProto\Lexicon\Record\App\Bsky\Feed\AbstractPost;
 use Revolution\Bluesky\Contracts\Recordable;
 use Revolution\Bluesky\Types\ReplyRef;
@@ -20,131 +17,48 @@ class Post extends AbstractPost implements Arrayable, Recordable
     use Macroable;
     use Conditionable;
 
-    public function __construct(string $text = '')
+    public function __construct(string $text = '', ?array $facets = null)
     {
         $this->text = $text;
+        $this->facets = $facets;
     }
 
-    public static function create(string $text = ''): static
+    public static function create(string $text = '', ?array $facets = null): static
     {
-        return new static(text: $text);
+        return new static($text, $facets);
     }
 
-    /**
-     * Append to existing text.
-     */
-    public function text(string $text): static
+    public function text(string $text = ''): static
     {
-        $this->text .= $text;
+        $this->text = $text;
 
         return $this;
     }
 
-    /**
-     * Add new lines to text.
-     */
-    public function newLine(int $count = 1): static
+    public function facets(?array $facets = null): static
     {
-        $this->text .= Str::repeat(PHP_EOL, $count);
+        $this->facets = $facets;
 
         return $this;
     }
 
-    /**
-     * Add mention facets.
-     */
-    public function mention(string $text, #[Format('did')] string $did): static
-    {
-        $this->facets[] = [
-            'index' => $this->buildFacetIndex($text),
-            'features' => [
-                [
-                    '$type' => Facet::Mention->value,
-                    'did' => $did,
-                ],
-            ],
-        ];
-
-        $this->text .= $text;
-
-        return $this;
-    }
-
-    /**
-     * Add link facets.
-     */
-    public function link(string $text, string $uri): static
-    {
-        $this->facets[] = [
-            'index' => $this->buildFacetIndex($text),
-            'features' => [
-                [
-                    '$type' => Facet::Link->value,
-                    'uri' => $uri,
-                ],
-            ],
-        ];
-
-        $this->text .= $text;
-
-        return $this;
-    }
-
-    /**
-     * Add tag facets.
-     */
-    public function tag(string $text, string $tag): static
-    {
-        $this->facets[] = [
-            'index' => $this->buildFacetIndex($text),
-            'features' => [
-                [
-                    '$type' => Facet::Tag->value,
-                    'tag' => $tag,
-                ],
-            ],
-        ];
-
-        $this->text .= $text;
-
-        return $this;
-    }
-
-    /**
-     * Add facet.
-     */
-    public function facet(array $facet): static
-    {
-        $this->facets[] = $facet;
-
-        return $this;
-    }
-
-    private function buildFacetIndex(string $text): array
-    {
-        return [
-            'byteStart' => $byteStart = strlen($this->text),
-            'byteEnd' => $byteStart + strlen($text),
-        ];
-    }
-
-    public function embed(array|Arrayable $embed): static
+    public function embed(null|array|Arrayable $embed = null): static
     {
         $this->embed = $embed instanceof Arrayable ? $embed->toArray() : $embed;
 
         return $this;
     }
 
-    public function langs(array $langs): static
+    public function langs(?array $langs = null): static
     {
         $this->langs = $langs;
 
         return $this;
     }
 
-    public function reply(ReplyRef $reply): static
+    public function reply(?ReplyRef $reply = null): static
     {
-        $this->reply = $reply->toArray();
+        $this->reply = $reply?->toArray();
 
         return $this;
     }
