bfab8be0f93a2945e585364f04c660dc6e539c3b
kawax <kawaxbiz@gmail.com>
2024-11-11 12:09:32 +0900

    Create StrongRef.php

---
+++ src/Types/StrongRef.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Revolution\Bluesky\Types;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Revolution\AtProto\Lexicon\Attributes\Format;
+
+final readonly class StrongRef implements Arrayable
+{
+    public function __construct(
+        protected string $uri,
+        protected string $cid,
+    ) {
+    }
+
+    /**
+     * @param  string  $uri  at://
+     */
+    public static function to(#[Format('at-uri')] string $uri, #[Format('cid')] string $cid): self
+    {
+        return new self($uri, $cid);
+    }
+
+    /**
+     * @return array{uri: string, cid: string}
+     */
+    public function toArray(): array
+    {
+        return [
+            'uri' => $this->uri,
+            'cid' => $this->cid,
+        ];
+    }
+}
