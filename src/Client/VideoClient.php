1da18d3b105433d86f6bfad2713054f3a035e751
kawax <kawaxbiz@gmail.com>
2024-11-14 11:11:34 +0900

    Update VideoClient.php

--- src/Client/VideoClient.php
+++ src/Client/VideoClient.php
@@ -7,13 +7,31 @@ use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
 use Revolution\AtProto\Lexicon\Attributes\Format;
 use Revolution\AtProto\Lexicon\Contracts\App\Bsky\Video;
+use Revolution\Bluesky\Client\Concerns\AppBskyVideo;
 
 class VideoClient
 {
     use Macroable;
     use Conditionable;
-
     use HasHttp;
+
+    public function getJobStatus(string $jobId): Response
+    {
+        return $this->call(
+            api: Video::getJobStatus,
+            method: self::GET,
+            params: compact($this->params(__METHOD__)),
+        );
+    }
+
+    public function getUploadLimits(): Response
+    {
+        return $this->call(
+            api: Video::getUploadLimits,
+            method: self::GET,
+            params: compact($this->params(__METHOD__)),
+        );
+    }
 
     public function uploadVideo(#[Format('did')] string $did, mixed $data, string $name, string $type = 'video/mp4'): Response
     {
