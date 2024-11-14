9d1d237580655578dbc6c9a6c9e4468c2a9d5435
kawax <kawaxbiz@gmail.com>
2024-11-15 08:02:44 +0900

    Update XrpcClient.php

--- src/Contracts/XrpcClient.php
+++ src/Contracts/XrpcClient.php
@@ -3,10 +3,11 @@
 namespace Revolution\Bluesky\Contracts;
 
 use Illuminate\Http\Client\PendingRequest;
+use Psr\Http\Message\StreamInterface;
 
 interface XrpcClient
 {
     public function withHttp(PendingRequest $http): static;
 
-    public function withBody($content, $contentType = 'image/png'): static;
+    public function withBody(StreamInterface|string $content, string $contentType = 'image/png'): static;
 }
