1a173927286a4c52e27d9ee761225b010e72e676
kawax <kawaxbiz@gmail.com>
2024-11-14 11:06:52 +0900

    Update VideoClient.php

--- src/Client/VideoClient.php
+++ src/Client/VideoClient.php
@@ -2,17 +2,27 @@
 
 namespace Revolution\Bluesky\Client;
 
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\Macroable;
+use Revolution\AtProto\Lexicon\Attributes\Format;
 use Revolution\AtProto\Lexicon\Contracts\App\Bsky\Video;
-use Revolution\Bluesky\Client\Concerns\AppBskyVideo;
 
-class VideoClient implements Video
+class VideoClient
 {
     use Macroable;
     use Conditionable;
 
     use HasHttp;
 
-    use AppBskyVideo;
+    public function uploadVideo(#[Format('did')] string $did, mixed $data, string $name, string $type = 'video/mp4'): Response
+    {
+        return $this->http()
+            ->withBody($data, $type)
+            ->withUrlParameters([
+                'did' => $did,
+                'name' => $name,
+            ])
+            ->post(Video::uploadVideo);
+    }
 }
