8ab3c3b01bd560e41b294d7a3896fc4a6d47c57b
kawax <kawaxbiz@gmail.com>
2024-11-16 13:57:31 +0900

    Update BlueskyRoute.php

--- src/Notifications/BlueskyRoute.php
+++ src/Notifications/BlueskyRoute.php
@@ -4,21 +4,36 @@ declare(strict_types=1);
 
 namespace Revolution\Bluesky\Notifications;
 
+use Revolution\AtProto\Lexicon\Attributes\Format;
 use Revolution\Bluesky\Session\OAuthSession;
 
 final readonly class BlueskyRoute
 {
+    /**
+     * @param  OAuthSession|null  $oauth
+     * @param  string|null  $identifier
+     * @param  string|null  $password
+     * @param  string|null  $receiver  Receiver DID when using PrivateChannel
+     */
     public function __construct(
         #[\SensitiveParameter] public ?OAuthSession $oauth = null,
         public ?string $identifier = null,
         #[\SensitiveParameter] public ?string $password = null,
+        #[Format('did')] public ?string $receiver = null,
     ) {
     }
 
+    /**
+     * @param  OAuthSession|null  $oauth
+     * @param  string|null  $identifier
+     * @param  string|null  $password
+     * @param  string|null  $receiver  Receiver DID when using PrivateChannel
+     */
     public static function to(
         #[\SensitiveParameter] ?OAuthSession $oauth = null,
         ?string $identifier = null,
         #[\SensitiveParameter] ?string $password = null,
+        #[Format('did')] ?string $receiver = null,
     ): self {
         return new self(...func_get_args());
     }
