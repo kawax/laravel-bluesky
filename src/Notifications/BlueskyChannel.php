a520ec95a1113da919de9133ee6cdc1305b04a39
kawax <kawaxbiz@gmail.com>
2024-11-17 18:49:35 +0900

    Update BlueskyChannel.php

--- src/Notifications/BlueskyChannel.php
+++ src/Notifications/BlueskyChannel.php
@@ -6,6 +6,7 @@ namespace Revolution\Bluesky\Notifications;
 
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Client\RequestException;
+use Illuminate\Http\Client\Response;
 use Illuminate\Notifications\Notification;
 use Revolution\Bluesky\Facades\Bluesky;
 use Revolution\Bluesky\Record\Post;
@@ -16,7 +17,7 @@ class BlueskyChannel
      * @throws RequestException
      * @throws AuthenticationException
      */
-    public function send(mixed $notifiable, Notification $notification): void
+    public function send(mixed $notifiable, Notification $notification): ?Response
     {
         /**
          * @var Post $post
@@ -25,24 +26,22 @@ class BlueskyChannel
         $post = $notification->toBluesky($notifiable);
 
         if (! $post instanceof Post) {
-            return; // @codeCoverageIgnore
+            return null; // @codeCoverageIgnore
         }
 
         /** @var BlueskyRoute $route */
         $route = $notifiable->routeNotificationFor('bluesky', $notification);
 
         if (! $route instanceof BlueskyRoute) {
-            return; // @codeCoverageIgnore
+            return null; // @codeCoverageIgnore
         }
 
-        match (true) {
+        return match (true) {
             $route->isOAuth() => Bluesky::withToken($route->oauth)
                 ->refreshSession()
-                ->post($post)
-                ->throw(),
+                ->post($post),
             $route->isLegacy() => Bluesky::login($route->identifier, $route->password)
-                ->post($post)
-                ->throw(),
+                ->post($post),
         };
     }
 }
