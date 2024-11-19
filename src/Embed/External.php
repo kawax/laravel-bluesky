dd947ca7a72d922278ba4505cb99eb5299b1f1bb
kawax <kawaxbiz@gmail.com>
2024-11-19 09:26:06 +0900

    Update External.php

--- src/Embed/External.php
+++ src/Embed/External.php
@@ -6,17 +6,36 @@ namespace Revolution\Bluesky\Embed;
 
 use Closure;
 use Illuminate\Contracts\Support\Arrayable;
+use Revolution\AtProto\Lexicon\Attributes\Blob;
+use Revolution\AtProto\Lexicon\Attributes\Format;
 use Revolution\AtProto\Lexicon\Enum\Embed;
 use Revolution\AtProto\Lexicon\Types\AbstractUnion;
 use Revolution\Bluesky\Types\BlobRef;
 
+/**
+ * External link / Social card.
+ *
+ * ```
+ * use Revolution\Bluesky\Embed\External;
+ * use Revolution\Bluesky\Record\Post;
+ *
+ * $external = External::create(
+ *     title: 'Title',
+ *     description: 'Description',
+ *     uri: 'https://',
+ *     thumb: fn() => Bluesky::uploadBlob(Storage::get('thumb.png'), Storage::mimeType('thumb.png'))->json('blob'),
+ * );
+ *
+ * $post = Post::create('test')->embed($external);
+ * ```
+ */
 final class External extends AbstractUnion implements Arrayable
 {
     public function __construct(
         private readonly string $title,
         private readonly string $description,
-        private readonly string $uri,
-        private readonly null|array|BlobRef|Closure $thumb = null,
+        #[Format('uri')] private readonly string $uri,
+        #[Blob(accept: ['image/*'], maxSize: 1000000)] private readonly null|array|BlobRef|Closure $thumb = null,
     ) {
         $this->type = Embed::External->value;
     }
