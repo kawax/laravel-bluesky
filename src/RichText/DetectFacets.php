fe836b136b95a31a251559b3f414632b435bc2c1
puklipo <pcs.engineer.team@gmail.com>
2024-11-20 08:50:16 +0900

    Update DetectFacets.php

--- src/RichText/DetectFacets.php
+++ src/RichText/DetectFacets.php
@@ -45,125 +45,107 @@ class DetectFacets
 
     protected function mention(): void
     {
-        $start = 0;
+        preg_match_all(self::MENTION_REGEX, $this->text, $matches, flags: PREG_OFFSET_CAPTURE);
 
-        while (preg_match(self::MENTION_REGEX, $this->text, $matches, offset: $start) !== 0) {
-            $handle = data_get($matches, 3);
+        collect(data_get($matches, 3))
+            ->each(function ($match) {
+                $handle = data_get($match, 0);
+                $start = data_get($match, 1);
 
-            if (empty($handle)) {
-                break;
-            }
+                if (empty($handle) || ! Identity::isHandle($handle)) {
+                    return;
+                }
 
-            $start = strpos($this->text, $handle, offset: $start + 1) - 1;
+                $did = Bluesky::resolveHandle($handle)->json('did');
 
-            if (! Identity::isHandle($handle)) {
-                $start++;
-                continue;
-            }
+                if (! Identity::isDID($did)) {
+                    return;
+                }
 
-            $did = Bluesky::resolveHandle($handle)->json('did');
-
-            if (! Identity::isDID($did)) {
-                $start++;
-                continue;
-            }
-
-            $this->facets[] = [
-                '$type' => self::TYPE,
-                'index' => [
-                    'byteStart' => $start,
-                    'byteEnd' => $start + strlen($handle) + 1,
-                ],
-                'features' => [
-                    [
-                        '$type' => Facet::Mention->value,
-                        'did' => $did,
+                $this->facets[] = [
+                    '$type' => self::TYPE,
+                    'index' => [
+                        'byteStart' => $start - 1,
+                        'byteEnd' => $start + strlen($handle),
                     ],
-                ],
-            ];
-
-            $start++;
-        }
+                    'features' => [
+                        [
+                            '$type' => Facet::Mention->value,
+                            'did' => $did,
+                        ],
+                    ],
+                ];
+            });
     }
 
     protected function link(): void
     {
-        $start = 0;
+        preg_match_all(self::URL_REGEX, $this->text, $matches, flags: PREG_OFFSET_CAPTURE);
 
-        while (preg_match(self::URL_REGEX, $this->text, $matches, offset: $start) !== 0) {
-            $uri = data_get($matches, 2);
+        collect(data_get($matches, 2))
+            ->each(function ($match) {
+                $uri = data_get($match, 0);
+                $start = data_get($match, 1);
+                $end = $start + strlen($uri);
 
-            if (empty($uri)) {
-                break;
-            }
+                if (! Str::startsWith($uri, 'http')) {
+                    return;
+                }
 
-            $start = strpos($this->text, $uri, offset: $start);
-            $end = $start + strlen($uri);
+                $uri = Str::of($uri)->whenTest('/[.,;:!?]$/', function (Stringable $string) use (&$end) {
+                    $end--;
+                    return $string->rtrim('.,;:!?');
+                })->toString();
 
-            if (! Str::startsWith($uri, 'http')) {
-                $start++;
-                continue;
-            }
+                $uri = Str::of($uri)->whenTest('/[)]$/', function (Stringable $string) use (&$end) {
+                    $end--;
+                    return $string->rtrim(')');
+                })->toString();
 
-            $uri = Str::of($uri)->whenTest('/[.,;:!?]$/', function (Stringable $string) use (&$end) {
-                $end--;
-                return $string->rtrim('.,;:!?');
-            })->toString();
-
-            $uri = Str::of($uri)->whenTest('/[)]$/', function (Stringable $string) use (&$end) {
-                $end--;
-                return $string->rtrim(')');
-            })->toString();
-
-            $this->facets[] = [
-                '$type' => self::TYPE,
-                'index' => [
-                    'byteStart' => $start,
-                    'byteEnd' => $end,
-                ],
-                'features' => [
-                    [
-                        '$type' => Facet::Link->value,
-                        'uri' => $uri,
+                $this->facets[] = [
+                    '$type' => self::TYPE,
+                    'index' => [
+                        'byteStart' => $start,
+                        'byteEnd' => $end,
                     ],
-                ],
-            ];
-
-            $start++;
-        }
+                    'features' => [
+                        [
+                            '$type' => Facet::Link->value,
+                            'uri' => $uri,
+                        ],
+                    ],
+                ];
+            });
     }
 
     protected function tag(): void
     {
-        $start = 0;
+        preg_match_all(self::TAG_REGEX, $this->text, $matches, flags: PREG_OFFSET_CAPTURE);
 
-        while (preg_match(self::TAG_REGEX, $this->text, $matches, offset: $start) !== 0) {
-            $tag = data_get($matches, 2);
+        collect(data_get($matches, 2))
+            ->each(function ($match) {
+                $tag = data_get($match, 0);
+                $start = data_get($match, 1) - 1;
 
-            $tag = Str::of($tag)->replaceMatches(self::TRAILING_PUNCTUATION_REGEX, '')->toString();
+                $tag = Str::of($tag)->replaceMatches(self::TRAILING_PUNCTUATION_REGEX, '')->toString();
 
-            if (empty($tag) || strlen($tag) > 64) {
-                $start++;
-                continue;
-            }
+                if (empty($tag) || strlen($tag) > 64) {
+                    return;
+                }
 
-            $start = strpos($this->text, '#'.$tag, offset: $start);
-
-            $this->facets[] = [
-                '$type' => self::TYPE,
-                'index' => [
-                    'byteStart' => $start,
-                    'byteEnd' => $start + 1 + strlen($tag),
-                ],
-                'features' => [
-                    [
-                        '$type' => Facet::Tag->value,
-                        'tag' => $tag,
+                $this->facets[] = [
+                    '$type' => self::TYPE,
+                    'index' => [
+                        'byteStart' => $start,
+                        'byteEnd' => $start + 1 + strlen($tag),
                     ],
-                ],
-            ];
-
-            $start++;
-        }
+                    'features' => [
+                        [
+                            '$type' => Facet::Tag->value,
+                            'tag' => $tag,
+                        ],
+                    ],
+                ];
+            });
     }
 }
