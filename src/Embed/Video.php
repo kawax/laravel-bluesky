15f66a3f98deef7d4d399e4118e182f1ee4aa4c7
kawax <kawaxbiz@gmail.com>
2024-11-19 10:28:55 +0900

    Update Video.php

--- src/Embed/Video.php
+++ src/Embed/Video.php
@@ -5,19 +5,21 @@ declare(strict_types=1);
 namespace Revolution\Bluesky\Embed;
 
 use Illuminate\Contracts\Support\Arrayable;
-use Revolution\AtProto\Lexicon\Enum\Embed;
-use Revolution\AtProto\Lexicon\Types\AbstractUnion;
 use Revolution\Bluesky\Types\BlobRef;
+use Revolution\AtProto\Lexicon\Union\App\Bsky\Embed\AbstractVideo;
 
-final class Video extends AbstractUnion implements Arrayable
+final class Video extends AbstractVideo implements Arrayable
 {
     public function __construct(
-        private readonly BlobRef|array $video,
-        private readonly ?string $alt = null,
-        private readonly ?array $captions = null,
-        private readonly ?array $aspectRatio = null,
+        BlobRef|array $video,
+        ?string $alt = null,
+        ?array $captions = null,
+        ?array $aspectRatio = null,
     ) {
-        $this->type = Embed::Video->value;
+        $this->video = $video instanceof BlobRef ? $video->toArray() : $video;
+        $this->alt = $alt;
+        $this->captions = $captions;
+        $this->aspectRatio = $aspectRatio;
     }
 
     public static function create(BlobRef|array $video, ?string $alt = null, ?array $captions = null, ?array $aspectRatio = null): self
@@ -28,8 +30,8 @@ final class Video extends AbstractUnion implements Arrayable
     public function toArray(): array
     {
         return collect([
-            '$type' => $this->type,
-            'video' => $this->video instanceof BlobRef ? $this->video->toArray() : $this->video,
+            '$type' => self::NSID,
+            'video' => $this->video,
             'alt' => $this->alt,
             'captions' => $this->captions,
             'aspectRatio' => $this->aspectRatio,
