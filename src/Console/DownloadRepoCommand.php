b2b3b46a91981457740582dd409bea0d654b0517
kawax <kawaxbiz@gmail.com>
2024-11-19 14:34:11 +0900

    Create DownloadRepoCommand.php

---
+++ src/Console/DownloadRepoCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Revolution\Bluesky\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Revolution\AtProto\Lexicon\Contracts\Com\Atproto\Sync;
+use Revolution\Bluesky\Facades\Bluesky;
+use Revolution\Bluesky\Support\DidDocument;
+use Revolution\Bluesky\Support\Identity;
+
+/**
+ * Sample command to download the actor's "car" file. Does not include parsing the Car file.
+ *
+ * ```
+ * php artisan bluesky:download-repo ***.bsky.social
+ * ```
+ *
+ * @see https://docs.bsky.app/blog/repo-export
+ */
+class DownloadRepoCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bluesky:download-repo {actor : DID or handle}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Download actor\'s car file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $actor = $this->argument('actor');
+
+        $this->line('Actor: '.$actor);
+
+        if (Identity::isHandle($actor)) {
+            $did = Bluesky::resolveHandle($actor)->json('did');
+        } else {
+            $did = $actor;
+        }
+
+        if (! Identity::isDID($did)) {
+            $this->error('Invalid actor');
+            return 1;
+        }
+
+        $this->line('DID: '.$did);
+
+        $didDoc = DidDocument::make()->fetch($did);
+
+        $pds = $didDoc->pdsUrl();
+
+        $this->line('PDS: '.$pds);
+
+        $response = Http::baseUrl($pds.'/xrpc/')
+            ->get(Sync::getRepo, [
+                'did' => $did,
+            ])->throw();
+
+        if ($response->successful()) {
+            $file = 'bluesky/download/'.Str::slug($actor, dictionary: ['.' => '-', ':' => '-']).'.car';
+            Storage::disk('local')->put($file, $response->body());
+
+            $this->info('Download successful: '.Storage::disk('local')->path($file));
+        }
+
+        return 0;
+    }
+}
