a20b7d5d7695d67a7b574bf67024832276dc65a7
kawax <kawaxbiz@gmail.com>
2024-11-19 10:32:17 +0900

    Update StrongRef.php

--- src/Types/StrongRef.php
+++ src/Types/StrongRef.php
@@ -7,6 +7,9 @@ use Revolution\AtProto\Lexicon\Attributes\Format;
 use Revolution\AtProto\Lexicon\Attributes\NSID;
 use Revolution\AtProto\Lexicon\Union\Com\Atproto\Repo\AbstractStrongRef;
 
+/**
+ * A URI with a content-hash fingerprint.
+ */
 #[NSID('com.atproto.repo.strongRef')]
 final class StrongRef extends AbstractStrongRef implements Arrayable
 {
@@ -17,7 +20,9 @@ final class StrongRef extends AbstractStrongRef implements Arrayable
     }
 
     /**
-     * @param  string  $uri  at://
+     * ```
+     * StrongRef::to(uri: 'at://', cid: '...');
+     * ```
      */
     public static function to(#[Format('at-uri')] string $uri, #[Format('cid')] string $cid): self
     {
