f11d07fc9ed177ba8243ff1653aa65fa39572514
kawax <kawaxbiz@gmail.com>
2024-11-23 10:31:44 +0900

    Update FeedSkeletonController.php

--- src/FeedGenerator/Http/FeedSkeletonController.php
+++ src/FeedGenerator/Http/FeedSkeletonController.php
@@ -3,8 +3,10 @@
 namespace Revolution\Bluesky\FeedGenerator\Http;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Revolution\AtProto\Lexicon\Enum\Feed;
 use Revolution\Bluesky\FeedGenerator\FeedGenerator;
+use Revolution\Bluesky\Socalite\Key\JsonWebToken;
 use Revolution\Bluesky\Support\AtUri;
 
 class FeedSkeletonController
@@ -21,7 +23,26 @@ class FeedSkeletonController
             name: $at->rkey(),
             limit: $request->input('limit'),
             cursor: $request->input('cursor'),
+            user: $this->userDid($request),
             request: $request,
         );
+    }
+
+    /**
+     * Requesting user's DID.
+     *
+     * Skip verify.
+     */
+    protected function userDid(Request $request): ?string
+    {
+        $jwt = Str::of($request->header('Authorization'))->chopStart('Bearer')->trim()->toString();
+
+        if (empty($jwt)) {
+            return null;
+        }
+
+        $payload = data_get(JsonWebToken::decode($jwt), 'payload');
+
+        return data_get($payload, 'iss');
     }
 }
