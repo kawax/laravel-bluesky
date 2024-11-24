3ecf5e9481feb11591748240557e2a8c5020dbba
kawax <kawaxbiz@gmail.com>
2024-11-24 09:06:51 +0900

    Create WellKnownConfig.php

---
+++ src/WellKnown/WellKnownConfig.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Revolution\Bluesky\WellKnown;
+
+use Closure;
+use Illuminate\Support\Str;
+use Revolution\Bluesky\Support\DID;
+
+class WellKnownConfig
+{
+    protected static ?Closure $didUsing = null;
+
+    protected static ?Closure $atprotoDidUsing = null;
+
+    /**
+     * Overrides the `/.well-known/did.json` response.
+     *
+     * ```
+     * // AppServiceProvider::boot()
+     *
+     * WellKnownConfig::didUsing(function () {
+     *     return [];
+     * });
+     * ```
+     */
+    public static function didUsing(?Closure $callback): void
+    {
+        static::$didUsing = $callback;
+    }
+
+    /**
+     * Default is a setting for FeedGenerator.
+     */
+    public static function did(): mixed
+    {
+        if (is_callable(static::$didUsing)) {
+            return call_user_func(static::$didUsing);
+        }
+
+        return [
+            '@context' => [
+                'https://www.w3.org/ns/did/v1',
+            ],
+            'id' => config('bluesky.generator.service') ?? DID::web(),
+            'service' => [
+                [
+                    'id' => '#bsky_fg',
+                    'type' => 'BskyFeedGenerator',
+                    'serviceEndpoint' => Str::rtrim(url('/'), '/'),
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Overrides the `/.well-known/atproto-did` response.
+     *
+     * ```
+     * // AppServiceProvider::boot()
+     *
+     * WellKnownConfig::atprotoDidUsing(fn() => 'did:plc:***');
+     * ```
+     */
+    public static function atprotoDidUsing(?Closure $callback): void
+    {
+        static::$atprotoDidUsing = $callback;
+    }
+
+    /**
+     * Default is empty, since it can also be set by DNS.
+     */
+    public static function atprotoDid(): mixed
+    {
+        if (is_callable(static::$atprotoDidUsing)) {
+            return call_user_func(static::$atprotoDidUsing);
+        }
+
+        return '';
+    }
+}
