5b65d0e1b8882893b3e1e4288b3c0f92ac560901
kawax <kawaxbiz@gmail.com>
2024-11-24 09:06:54 +0900

    Create WellKnownController.php

---
+++ src/WellKnown/Http/WellKnownController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Revolution\Bluesky\WellKnown\Http;
+
+use Revolution\Bluesky\WellKnown\WellKnownConfig;
+
+class WellKnownController
+{
+    /**
+     * `/.well-known/did.json`
+     */
+    public function did()
+    {
+        return WellKnownConfig::did();
+    }
+
+    /**
+     * `/.well-known/atproto-did`
+     */
+    public function atproto()
+    {
+        return WellKnownConfig::atprotoDid();
+    }
+}
