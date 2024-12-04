2b877e22787351f5c48146ef32d2449e8ac27dd4
kawax <kawaxbiz@gmail.com>
2024-12-04 21:10:19 +0900

    Update Identity.php

--- src/Support/Identity.php
+++ src/Support/Identity.php
@@ -15,13 +15,17 @@ use Revolution\Bluesky\Facades\Bluesky;
  */
 class Identity
 {
-    // "alice.test"
+    // "***.bsky.social" "alice.test"
     protected const HANDLE_REGEX = '/^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$/';
 
     // "did:plc:1234..." "did:web:alice.test"
     protected const DID_REGEX = '/^did:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._-]$/';
 
     protected const PLC_DIRECTORY = 'https://plc.directory';
+
+    public const CACHE_HANDLE = 'bluesky:resolve-handle:';
+
+    public const CACHE_DID = 'bluesky:resolve-did:';
 
     public static function isHandle(?string $handle): bool
     {
@@ -37,11 +41,11 @@ class Identity
      * @param  string|null  $id  handle or DID.  e.g. "alice.test" "did:plc:1234..." "did:web:alice.test"
      * @return Response{id: string, alsoKnownAs: array, verificationMethod: array, service: array} didDoc
      */
-    public function resolveIdentity(?string $id): Response
+    public function resolveIdentity(?string $id, bool $cache = true): Response
     {
         return match (true) {
-            self::isHandle($id) => $this->resolveDid($this->resolveHandle($id)),
-            self::isDID($id) => $this->resolveDid($id),
+            self::isHandle($id) => $this->resolveDid($this->resolveHandle($id, $cache), $cache),
+            self::isDID($id) => $this->resolveDid($id, $cache),
             default => throw new InvalidArgumentException('Invalid ID provided.'),
         };
     }
@@ -58,10 +62,14 @@ class Identity
      * @param  string|null  $handle  e.g. "alice.test"
      * @return string|null DID
      */
-    public function resolveHandle(?string $handle): ?string
+    public function resolveHandle(?string $handle, bool $cache = true): ?string
     {
         if (! self::isHandle($handle)) {
             throw new InvalidArgumentException("The handle '$handle' is not a valid handle."); // @codeCoverageIgnore
+        }
+
+        if ($cache && cache()->has(self::CACHE_HANDLE.$handle)) {
+            return cache(self::CACHE_HANDLE.$handle);
         }
 
         // check DND TXT record
@@ -74,6 +82,10 @@ class Identity
         $did = Str::chopStart($did, 'did=');
 
         if (self::isDID($did)) {
+            if ($cache) {
+                cache()->put(self::CACHE_HANDLE.$handle, $did, now()->addDay());
+            }
+
             return $did;
         }
 
@@ -82,6 +94,10 @@ class Identity
         $did = trim($response->body());
 
         if (self::isDID($did)) {
+            if ($cache) {
+                cache()->put(self::CACHE_HANDLE.$handle, $did, now()->addDay());
+            }
+
             return $did;
         }
 
@@ -97,10 +113,14 @@ class Identity
      * @return Response{id: string, alsoKnownAs: array, verificationMethod: array, service: array} didDoc
      * @link https://plc.directory/did:plc:ewvi7nxzyoun6zhxrhs64oiz
      */
-    public function resolveDID(?string $did): Response
+    public function resolveDID(?string $did, bool $cache = true): Response
     {
         if (! self::isDID($did)) {
             throw new InvalidArgumentException("The did '$did' is not a valid DID.");
+        }
+
+        if ($cache && cache()->has(self::CACHE_DID.$did)) {
+            return new Response(Http::response(cache(self::CACHE_DID.$did))->wait());
         }
 
         $url = match (true) {
@@ -109,6 +129,12 @@ class Identity
             default => throw new InvalidArgumentException('Unsupported DID type'),
         };
 
-        return Http::get($url);
+        $response = Http::get($url);
+
+        if ($cache && $response->successful()) {
+            cache()->put(self::CACHE_DID.$did, $response->json(), now()->addDay());
+        }
+
+        return $response;
     }
 }
