7aa68631f46d34bb7ac9446da79f59019c98bdb5
kawax <kawaxbiz@gmail.com>
2024-12-09 11:31:14 +0900

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Foundation\Application;
 use Laravel\Socialite\SocialiteServiceProvider;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Revolution\Bluesky\Providers\BlueskyServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
+    use WithWorkbench;
+
     /**
      * Load package service provider.
      *
@@ -45,9 +49,10 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      */
     protected function defineEnvironment($app): void
     {
-        $app['config']->set('bluesky.oauth', [
+        tap($app['config'], function (Repository $config) {
             //for testing
-            'private_key' => 'LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tDQpNSUdIQWdFQU1CTUdCeXFHU000OUFnRUdDQ3FHU000OUF3RUhCRzB3YXdJQkFRUWdEZkhxckZzSkRUUkVMa1ZIDQpHNG9DNTh2U2k0SnFkN3NPSTVNdTZzeVdrY21oUkFOQ0FBVHc2VU1DMlpYcFV4blhFc1BjRTA5aFdoYWdLbWxODQpRUXovSUlFYUVHdEFtSU5YeGUzTHZ0NE5KUS9YVWdGV3hkdEJBbUhQcFN4MlM3RnIvdmFhT2UzZw0KLS0tLS1FTkQgUFJJVkFURSBLRVktLS0tLQ',
-        ]);
+            $config->set('bluesky.oauth.private_key', 'LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tDQpNSUdIQWdFQU1CTUdCeXFHU000OUFnRUdDQ3FHU000OUF3RUhCRzB3YXdJQkFRUWdEZkhxckZzSkRUUkVMa1ZIDQpHNG9DNTh2U2k0SnFkN3NPSTVNdTZzeVdrY21oUkFOQ0FBVHc2VU1DMlpYcFV4blhFc1BjRTA5aFdoYWdLbWxODQpRUXovSUlFYUVHdEFtSU5YeGUzTHZ0NE5KUS9YVWdGV3hkdEJBbUhQcFN4MlM3RnIvdmFhT2UzZw0KLS0tLS1FTkQgUFJJVkFURSBLRVktLS0tLQ',
+            );
+        });
     }
 }
