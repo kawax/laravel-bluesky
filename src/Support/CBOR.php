8ff9707dc59c7b6ac3c7659add8a5692c9b2c715
puklipo <pcs.engineer.team@gmail.com>
2024-12-13 09:30:32 +0900

    Update CBOR.php

--- src/Support/CBOR.php
+++ src/Support/CBOR.php
@@ -4,53 +4,39 @@ declare(strict_types=1);
 
 namespace Revolution\Bluesky\Support;
 
+use CBOR\ByteStringObject;
 use CBOR\CBORObject;
 use CBOR\Decoder;
+use CBOR\IndefiniteLengthByteStringObject;
+use CBOR\IndefiniteLengthListObject;
+use CBOR\IndefiniteLengthMapObject;
+use CBOR\IndefiniteLengthTextStringObject;
 use CBOR\ListObject;
 use CBOR\MapObject;
+use CBOR\NegativeIntegerObject;
+use CBOR\OtherObject\FalseObject;
+use CBOR\OtherObject\NullObject;
+use CBOR\OtherObject\TrueObject;
 use CBOR\StringStream;
 use CBOR\TextStringObject;
 use CBOR\UnsignedIntegerObject;
-use Illuminate\Support\Arr;
 use InvalidArgumentException;
 
 final class CBOR
 {
-    public static function fromArray(array $data): CBORObject
+    //public const FLOAT_FORMAT_SINGLE_PRECISION = 0b0000001;
+
+    public const INDEFINITE_BYTE_STRING_LENGTH = 0b00000010;
+
+    public const INDEFINITE_TEXT_STRING_LENGTH = 0b00000100;
+
+    public const INDEFINITE_LIST_LENGTH = 0b00001000;
+
+    public const INDEFINITE_MAP_LENGTH = 0b00010000;
+
+    public static function fromArray(array $data): string
     {
-        if (Arr::isAssoc($data)) {
-            $cbor = MapObject::create();
-        } elseif (Arr::isList($data)) {
-            $cbor = ListObject::create();
-        } else {
-            throw new InvalidArgumentException();
-        }
-
-        foreach ($data as $key => $value) {
-            if (is_string($key)) {
-                $name = TextStringObject::create($key);
-            } else {
-                unset($name);
-            }
-
-            if (is_string($value)) {
-                $val = TextStringObject::create($value);
-            } elseif (is_int($value)) {
-                $val = UnsignedIntegerObject::create($value);
-            } elseif (is_array($value)) {
-                $val = self::fromArray($value);
-            }
-
-            if (isset($val)) {
-                if (isset($name)) {
-                    $cbor->add($name, $val);
-                } else {
-                    $cbor->add($val);
-                }
-            }
-        }
-
-        return $cbor;
+        return (new self())->encode($data);
     }
 
     public static function decode(string $data): CBORObject
@@ -58,5 +44,89 @@ final class CBOR
         $decoder = Decoder::create();
 
         return $decoder->decode(StringStream::create($data));
+    }
+
+    /**
+     * Temporarily copied from cbor-php.
+     * Replace after 4.0 release.
+     *
+     * @link https://github.com/Spomky-Labs/cbor-php/blob/4.0.x/src/Encoder.php
+     */
+    protected function encode(mixed $data, int $options = 0): string
+    {
+        return (string) $this->processData($data, $options);
+    }
+
+    private function processData(mixed $data, int $option): CBORObject
+    {
+        return match (true) {
+            $data instanceof CBORObject => $data,
+            is_string($data) => preg_match('//u', $data) === 1 ? $this->processTextString(
+                $data,
+                $option
+            ) : $this->processByteString($data, $option),
+            is_array($data) => array_is_list($data) ? $this->processList($data, $option) : $this->processMap(
+                $data,
+                $option
+            ),
+            is_int($data) => $data < 0 ? NegativeIntegerObject::create($data) : UnsignedIntegerObject::create($data),
+            //is_float($data) => $this->processFloat($data, $option),
+            $data === null => NullObject::create(),
+            $data === false => FalseObject::create(),
+            $data === true => TrueObject::create(),
+            default => throw new InvalidArgumentException('Unsupported data type'),
+        };
+    }
+
+    /**
+     * @param  array<array-key, mixed>  $data
+     */
+    private function processList(array $data, int $option): ListObject|IndefiniteLengthListObject
+    {
+        $isIndefinite = 0 !== ($option & self::INDEFINITE_LIST_LENGTH);
+        $list = $isIndefinite ? IndefiniteLengthListObject::create() : ListObject::create();
+        foreach ($data as $item) {
+            $list->add($this->processData($item, $option));
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param  array<array-key, mixed>  $data
+     */
+    private function processMap(array $data, int $option): MapObject|IndefiniteLengthMapObject
+    {
+        $isIndefinite = 0 !== ($option & self::INDEFINITE_MAP_LENGTH);
+        $map = $isIndefinite ? IndefiniteLengthMapObject::create() : MapObject::create();
+        foreach ($data as $key => $value) {
+            $map->add($this->processData($key, $option), $this->processData($value, $option));
+        }
+
+        return $map;
+    }
+
+    private function processTextString(string $data, int $option): TextStringObject|IndefiniteLengthTextStringObject
+    {
+        $isIndefinite = 0 !== ($option & self::INDEFINITE_TEXT_STRING_LENGTH);
+        $cbor = TextStringObject::create($data);
+
+        if (! $isIndefinite) {
+            return $cbor;
+        }
+
+        return IndefiniteLengthTextStringObject::create()->add($cbor);
+    }
+
+    private function processByteString(string $data, int $option): ByteStringObject|IndefiniteLengthByteStringObject
+    {
+        $isIndefinite = 0 !== ($option & self::INDEFINITE_BYTE_STRING_LENGTH);
+        $cbor = ByteStringObject::create($data);
+
+        if (! $isIndefinite) {
+            return $cbor;
+        }
+
+        return IndefiniteLengthByteStringObject::create()->add($cbor);
     }
 }
