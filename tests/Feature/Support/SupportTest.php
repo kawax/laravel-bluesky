6d30bee1cfa80c25c5b673eb539783adff4de83c
kawax <kawaxbiz@gmail.com>
2024-12-16 14:03:23 +0900

    Update SupportTest.php

--- tests/Feature/Support/SupportTest.php
+++ tests/Feature/Support/SupportTest.php
@@ -272,10 +272,17 @@ class SupportTest extends TestCase
         $this->assertSame('bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba', $cid);
     }
 
+    public function test_cid_encode_cbor()
+    {
+        $cid = CID::encode(CBOR::fromArray(['test' => 'test']), codec: CID::DAG_CBOR);
+
+        $this->assertSame('bafyreib3h3z3a5jwjcthjojoqjpzrlzly53ycpctnmfsijdk3qb5m3qcdq', $cid);
+    }
+
     public function test_cid_verify()
     {
         $this->assertTrue(CID::verify('test', 'bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba'));
-        $this->assertFalse(CID::verify('test2', 'bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba'));
+        $this->assertFalse(CID::verify('test2', 'bafkreie7q3iidccmpvszul7kudcvvuavuo7u6gzlbobczuk5nqk3b4akba', codec: null));
     }
 
     public function test_cid_decode()
@@ -286,6 +293,24 @@ class SupportTest extends TestCase
 
         $this->assertSame(CID::CID_V1, $cid['version']);
         $this->assertSame($hash, $cid['hash']);
+    }
+
+    public function test_cid_decode_cbor()
+    {
+        $cid = CID::decode('bafyreib3h3z3a5jwjcthjojoqjpzrlzly53ycpctnmfsijdk3qb5m3qcdq');
+
+        $this->assertSame(CID::CID_V1, $cid['version']);
+        $this->assertSame(CID::DAG_CBOR, $cid['codec']);
+    }
+
+    public function test_cid_decode_v0()
+    {
+        $decode = CID::decodeV0('zQmNX6Tffavsya4xgBi2VJQnSuqy9GsxongxZZ9uZBqp16d');
+
+        $this->assertSame(CID::CID_V0, $decode['version']);
+        $this->assertSame(CID::DAG_PB, $decode['codec']);
+        $this->assertSame(CID::SHA2_256, $decode['hash_algo']);
+        $this->assertSame('02acecc5de2438ea4126a3010ecb1f8a599c8eff22fff1a1dcffe999b27fd3de', $decode['hash']);
     }
 
     public function test_cid_encode_dag_cbor()
