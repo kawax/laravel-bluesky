4176a0e970be97878870d555bda7aa38e63481d1
kawax <kawaxbiz@gmail.com>
2024-12-17 13:04:52 +0900

    Update CBOR.php

--- src/Support/CBOR.php
+++ src/Support/CBOR.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace Revolution\Bluesky\Support;
 
 use GuzzleHttp\Psr7\Utils;
-use Revolution\Bluesky\Support\CBOR\BytesWrapper;
-use Revolution\Bluesky\Support\CBOR\CIDLinkWrapper;
 use Revolution\Bluesky\Support\CBOR\Decoder;
 use Revolution\Bluesky\Support\CBOR\Encoder;
+use Revolution\Bluesky\Support\CBOR\Normalizer;
 
 final class CBOR
 {
@@ -31,37 +30,8 @@ final class CBOR
         return $decoder->decode();
     }
 
-    /**
-     * Match the official go-repo-export output.
-     *
-     * @link https://github.com/bluesky-social/cookbook/tree/main/go-repo-export
-     *
-     * @todo
-     */
     public static function normalize(mixed $data): mixed
     {
-        if (is_array($data)) {
-            return collect($data)->map(function ($item, $key) {
-                if (in_array($key, ['ref', 'link'], true) && $item instanceof CIDLinkWrapper) {
-                    return $item->toArray();
-                }
-                if (in_array($key, ['v', 't', 'l', 'data'], true) && $item instanceof CIDLinkWrapper) {
-                    return $item->mst();
-                }
-                if ($item instanceof CIDLinkWrapper) {
-                    return $item->cid();
-                }
-                if ($key === 'sig' && $item instanceof BytesWrapper) {
-                    return $item->encode();
-                }
-                if ($item instanceof BytesWrapper) {
-                    return $item->bytes();
-                }
-
-                return self::normalize($item);
-            })->toArray();
-        }
-
-        return $data;
+        return app()->call(Normalizer::class, compact('data'));
     }
 }
