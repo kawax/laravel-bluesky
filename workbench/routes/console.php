22d3258292f5987b2ecd13fbdb2ef4fe11e6455f
kawax <kawaxbiz@gmail.com>
2024-12-19 16:55:06 +0900

    Update console.php

--- workbench/routes/console.php
+++ workbench/routes/console.php
@@ -2,24 +2,27 @@
 
 declare(strict_types=1);
 
+use Illuminate\Console\Command;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Revolution\Bluesky\Facades\Bluesky;
 
-/** @var \Illuminate\Console\Command $this */
+/** @var Command $this */
 Artisan::command('inspire', function () {
-    /** @var \Illuminate\Console\Command $this */
+    /** @var Command $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
 // vendor/bin/testbench bsky:search "#bluesky"
 Artisan::command('bsky:search {q=#bluesky}', function () {
-    /** @var \Illuminate\Console\Command $this */
-    $response = Bluesky::searchPosts(q: $this->argument('q'), limit: 10);
+    /** @var Command $this */
+    /** @var string $q */
+    $q = $this->argument('q');
+    $response = Bluesky::searchPosts(q: $q, limit: 10);
 
     $response->collect('posts')
         ->each(function (array $post) {
-            /** @var \Illuminate\Console\Command $this */
+            /** @var Command $this */
             $this->table(
                 ['Name', 'Text'],
                 [[data_get($post, 'author.displayName'), data_get($post, 'record.text')]],
@@ -30,12 +33,14 @@ Artisan::command('bsky:search {q=#bluesky}', function () {
 
 // vendor/bin/testbench bsky:feed "bsky.app"
 Artisan::command('bsky:feed {actor}', function () {
-    /** @var \Illuminate\Console\Command $this */
-    $response = Bluesky::getAuthorFeed(actor: $this->argument('actor'), limit: 10);
+    /** @var Command $this */
+    /** @var string $actor */
+    $actor = $this->argument('actor');
+    $response = Bluesky::getAuthorFeed(actor: $actor, limit: 10);
 
     $response->collect('feed')
         ->each(function (array $post) {
-            /** @var \Illuminate\Console\Command $this */
+            /** @var Command $this */
             $this->table(
                 ['Name', 'Text'],
                 [[data_get($post, 'post.author.displayName'), data_get($post, 'post.record.text')]],
