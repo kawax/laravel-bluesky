91fd771562e6f0264e1f1211194da1cb472424b6
kawax <kawaxbiz@gmail.com>
2025-01-07 12:17:26 +0900

    lexicon

--- src/Client/Concerns/ToolsOzoneModeration.php
+++ src/Client/Concerns/ToolsOzoneModeration.php
@@ -66,7 +66,7 @@ trait ToolsOzoneModeration
         );
     }
 
-    public function queryEvents(?array $types = null, ?string $createdBy = null, ?string $sortDirection = 'desc', ?string $createdAfter = null, ?string $createdBefore = null, ?string $subject = null, ?array $collections = null, ?string $subjectType = null, ?bool $includeAllUserRecords = null, ?int $limit = 50, ?bool $hasComment = null, ?string $comment = null, ?array $addedLabels = null, ?array $removedLabels = null, ?array $addedTags = null, ?array $removedTags = null, ?array $reportTypes = null, ?string $cursor = null): Response
+    public function queryEvents(?array $types = null, ?string $createdBy = null, ?string $sortDirection = 'desc', ?string $createdAfter = null, ?string $createdBefore = null, ?string $subject = null, ?array $collections = null, ?string $subjectType = null, ?bool $includeAllUserRecords = null, ?int $limit = 50, ?bool $hasComment = null, ?string $comment = null, ?array $addedLabels = null, ?array $removedLabels = null, ?array $addedTags = null, ?array $removedTags = null, ?array $reportTypes = null, ?array $policies = null, ?string $cursor = null): Response
     {
         return $this->call(
             api: Moderation::queryEvents,
